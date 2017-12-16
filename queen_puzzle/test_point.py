caee497bd71976d2aa0a8341b044d4c44a59698e
Bima Wijaya <bimajatiwijaya@gmail.com>
2017-12-16 22:29:36 +0700

    [FIX] make test working

--- queen_puzzle/test_point.py
+++ queen_puzzle/test_point.py
@@ -4,6 +4,9 @@ import point
 class point_test(unittest.TestCase):
 
     def test_point(self):
-        point_1 = point.Point(4, 5)
-        self.assertEqual(43, point_1.get_x())
+        point_1 = point.Point()
+        point_1.set_point(4, 5)
+        self.assertEqual(4, point_1.get_x())
 
+if __name__ == '__main__':
+    unittest.main()
