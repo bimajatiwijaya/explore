dc9f54248e18378237b534a0fe1917c046e7f7eb
Bima Wijaya <bimajatiwijaya@gmail.com>
2017-12-16 10:43:18 +0700

    [add] point and block

---
+++ queen_puzzle/kotak_catur.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import point
+
+class c_block(point.Point):
+    army = ''
+    safe = True
+
+    def __init__(self, army=None, safe=True):
+        self.army = army
+        self.safe = safe
+    
+    def set_block_status(self, safe=True):
+        self.safe = safe
+    
+    def set_army(self, army=None):
+        if army:
+            self.army = army
+    
+    def get_block_status(self):
+        return self.safe
---
+++ queen_puzzle/point.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+class Point:
+    """ class point """
+    x = 0
+    y = 0
+
+    def __init__(self, x=0, y=0):
+        """ constructor """
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        """ get x """
+        return self.x
+
+    def get_y(self):
+        """ get y """
+        return self.y
